613973bc5faade3fd0424dea60adb501d6a415ab
hrems-dev <hrems077@gmail.com>
2025-11-19 18:47:08 -0800

    pagina de inicio

---
+++ resources/views/include/banner.blade.php
@@ -0,0 +1,17 @@
+<div class="flex items-center justify-evenly gap-10">
+    <div class="w-70 mx-4 border-5 rounded-full shadow-lg border-white">
+        <img class=" rounded-full " src="./img/candidato.jpg" alt="">
+    </div>
+    <div class="text-left">
+        <h1 class="text-5xl font-bold">RobEro nodado</h1>
+        <h3 class="text-gray-400">abogado</h3>
+        <br>
+        <p class="w-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur incidunt excepturi
+            soluta labore a, nisi consectetur aliquid numquam voluptate suscipit iste, adipisci, quis enim vitae. Harum
+            qui quam facilis sint!</p>
+        <button class="bg-blue-500 text-white px-4 py-2 rounded my-10" type="submit" cursor-pointer>ver mas informacion
+            <i class="fa-solid fa-arrow-right"></i></button>
+
+    </div>
+
+</div>
